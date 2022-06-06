45b294125b822136a51317bab0a044927364e5c0
deprov447 <ac.10edu@gmail.com>
2022-06-06 13:54:53 +0000

    CategoriesCard request added
    Signed-off-by: deprov447 <ac.10edu@gmail.com>

--- client/src/Categories/Categories.jsx
+++ client/src/Categories/Categories.jsx
@@ -1,28 +1,24 @@
 import CategoryCard from './CategoryCard';
-
-const categoriesArray = [
-  {
-    name: 'DemoCategory1',
-    img: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80',
-    desc: 'This is just a demo category, no real importance',
-  },
-  {
-    name: 'DemoCategory2',
-    img: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80',
-    desc: 'This is just a demo category, no real importance',
-  },
-  {
-    name: 'DemoCategory3',
-    img: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80',
-    desc: 'This is just a demo category, no real importance',
-  },
-];
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Categories = () => {
+  const [categoriesArray, setCategoriesArray] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/categories`)
+      .then(({ data }) => {
+        setCategoriesArray(data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <>
       {categoriesArray.map(({ name, img, desc }) => (
-        <CategoryCard name={name} img={img} desc={desc} />
+        <CategoryCard name={name} img={img} desc={desc} key={name} />
       ))}
     </>
   );
