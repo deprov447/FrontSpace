313d5debaeb2ef748456aa1f8aa359018306aaf0
deprov447 <ac.10edu@gmail.com>
2022-06-14 19:22:16 +0000

    New form route + fix
    Signed-off-by: deprov447 <ac.10edu@gmail.com>

--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const fetchCategoriesList = require("./business/fetchCategoriesList");
 const fetchTemplateForm = require("./business/fetchTemplateForm");
 const fetchTemplatesList = require("./business/fetchTemplatesList");
+const saveTemplateFormDetails = require("./business/saveTemplateFormDetails");
 const router = express();
 
 router.get("/categories", fetchCategoriesList);
 router.get("/categories/:category_name", fetchTemplatesList);
-router.get("/template/:template_name", fetchTemplateForm);
+router.get("/template/:template_id", fetchTemplateForm);
+router.post("/template/:template_id", saveTemplateFormDetails);
 
 module.exports = router;
