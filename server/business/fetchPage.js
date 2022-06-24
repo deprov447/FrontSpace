b3700d587d315d3aa2b0d78e54dc3a1292094021
deprov447 <ac.10edu@gmail.com>
2022-06-24 06:30:36 +0000

    404 for subdomain req
    Signed-off-by: deprov447 <ac.10edu@gmail.com>

--- server/business/fetchPage.js
+++ server/business/fetchPage.js
@@ -2,7 +2,10 @@ const Pages = require("../models/Pages");
 
 module.exports = async (req, res) => {
   const subdomain = req.headers["x-subdomain"];
-  const { templateId, formData } = await Pages.findOne({ subdomain });
-  console.log(templateId, formData);
-  res.render(`../templates/${templateId}/index.ejs`, { formData: formData });
+  const ret = await Pages.findOne({ subdomain });
+  if (ret == null) res.sendStatus(404);
+  else {
+    const { templateId, formData } = ret;
+    res.render(`../templates/${templateId}/index.ejs`, { formData: formData });
+  }
 };
